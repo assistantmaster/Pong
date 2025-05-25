9a9f12af6e12b795621b9adfb366235de59e1899
assistantmaster <140390211+assistantmaster@users.noreply.github.com>
2025-05-25 11:45:57 +0200

    Fenster ist funktionierend resizable

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 width = 1280
 height = 720
 
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("Pong")
 #pygame.display.set_icon(pygame.image.load("./images/favicon.png"))
 
@@ -20,15 +20,15 @@ copyright = font.render('© 2025 by Timo', True, (150,150,150))
 
 p1y = 300
 p2y = 300
-ballx = 630
-bally = 350
+ballx = screen.get_width()/2
+bally = screen.get_height()/2
 
 punkte1 = 0
 punkte2 = 0
 punkte1display = font3.render(f'Punkte: {punkte1}', True, (255,255,255))
 punkte2display = font3.render(f'Punkte: {punkte2}', True, (255,255,255))
 
-gamelength = 5
+gamelength = 60
 
 pspeed = 1
 if random.randint(0,1) == 0:
@@ -40,13 +40,13 @@ ballspeedy = random.uniform(-2*math.pi,2*math.pi)/10
 for i in range(3, 0, -1):
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 255, 255), (40, p1y, 10, 100))
-    pygame.draw.rect(screen, (255, 255, 255), (1230, p2y, 10, 100))
-    pygame.draw.circle(screen, (255, 255, 255), (ballx, bally),10)
+    pygame.draw.rect(screen, (255, 255, 255), (screen.get_width()-50, p2y, 10, 100))
+    pygame.draw.circle(screen, (255, 255, 255), (screen.get_width()/2, screen.get_height()/2),10)
     screen.blit(punkte1display, (20,20))
-    screen.blit(punkte2display, (1130,20))
+    screen.blit(punkte2display, (screen.get_width()-150,20))
     countdown_text = font2.render(str(i), True, (255, 255, 255))
-    screen.blit(countdown_text, (width // 2 - countdown_text.get_width() // 2, height // 2 - countdown_text.get_height() // 2))
-    screen.blit(copyright, (0,700))
+    screen.blit(countdown_text, (screen.get_width() // 2 - countdown_text.get_width() // 2, screen.get_height() // 2 - countdown_text.get_height() // 2))
+    screen.blit(copyright, (0,screen.get_height()-20))
     pygame.display.flip()
     pygame.time.delay(1000)
 
@@ -57,7 +57,7 @@ while running:
 
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 255, 255), (40, p1y, 10, 100))
-    pygame.draw.rect(screen, (255, 255, 255), (1230, p2y, 10, 100))
+    pygame.draw.rect(screen, (255, 255, 255), (screen.get_width()-50, p2y, 10, 100))
     pygame.draw.circle(screen, (255, 255, 255), (ballx, bally),10)
     currenttime = int(time.time() - starttime)
     timeleft = gamelength-currenttime
@@ -69,50 +69,50 @@ while running:
     punkte2display = font3.render(f'Punkte: {punkte2}', True, (255,255,255))
     timedisplay = font3.render(f'{timeleftmin}:{timeleftsec}', True, (255,255,255))
     screen.blit(punkte1display, (20,20))
-    screen.blit(punkte2display, (1130,20))
-    screen.blit(timedisplay, (width // 2 - timedisplay.get_width() // 2, 20))
-    screen.blit(copyright, (0,700))
+    screen.blit(punkte2display, (screen.get_width()-150,20))
+    screen.blit(timedisplay, (screen.get_width() // 2 - timedisplay.get_width() // 2, 20))
+    screen.blit(copyright, (0,screen.get_height()-20))
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w] and p1y > 0:
         p1y -= pspeed
     
-    if keys[pygame.K_s] and p1y < 620:
+    if keys[pygame.K_s] and p1y < screen.get_height()-100:
         p1y += pspeed
     
     if keys[pygame.K_UP] and p2y > 0:
         p2y -= pspeed
     
-    if keys[pygame.K_DOWN] and p2y < 620:
+    if keys[pygame.K_DOWN] and p2y < screen.get_height()-100:
         p2y += pspeed
 
     ballx -= ballspeedx
     bally -= ballspeedy
     
-    if bally <= 10 or bally >= 710:
+    if bally <= 10 or bally >= screen.get_height()-10:
         ballspeedy = -ballspeedy
 
     if ballx <= 50 and ballx > 40 and p1y <= bally <= p1y + 100:
         ballspeedx = -ballspeedx
         ballspeedy += random.randint(-10,10)/10
 
-    if ballx >= 1230 and ballx < 1240 and p2y <= bally <= p2y + 100:
+    if ballx >= screen.get_width()-50 and ballx < screen.get_width()-40 and p2y <= bally <= p2y + 100:
         ballspeedx = -ballspeedx
         ballspeedy += random.randint(-10,10)/10
 
     
     if ballx < 0:
         punkte2 += 1
-        ballx = 630
-        bally = 350
+        ballx = screen.get_width()/2
+        bally = screen.get_height()/2
         ballspeedx = 1
         ballspeedy = random.uniform(-2*math.pi,2*math.pi)/10
 
-    if ballx > 1280:
+    if ballx > screen.get_width():
         punkte1 += 1
-        ballx = 630
-        bally = 350
+        ballx = screen.get_width()/2
+        bally = screen.get_height()/2
         ballspeedx = -1
         ballspeedy = random.uniform(-2*math.pi,2*math.pi)/10
 
@@ -123,7 +123,7 @@ while running:
             winner_text = font2.render("Spieler 1 gewinnt!", True, (255, 255, 255))
         else:
             winner_text = font2.render("Spieler 2 gewinnt!", True, (255, 255, 255))
-        screen.blit(winner_text, (width // 2 - winner_text.get_width() // 2, 100))
+        screen.blit(winner_text, (screen.get_width() // 2 - winner_text.get_width() // 2, 100))
         pygame.display.flip()
         pygame.time.delay(3000)
         running = False
